008454d67c2023aee91df8e576182d1e43c6730d
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2021-09-10 01:30:23 +0900

    edit layout

---
+++ TodoAppLiT/Controller/MemoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MemoViewController.swift
+//  TodoAppLiT
+//
+//  Created by 河村大介 on 2021/09/10.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ TodoAppLiT/View/FirstViewTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewTableViewCell.swift
+//  TodoAppLiT
+//
+//  Created by 河村大介 on 2021/09/10.
+//
+
+import UIKit
+
+class FirstViewTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var memoLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setCell(memo: String, date: String) {
+        self.memoLabel.text = memo
+        self.dateLabel.text = date
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
