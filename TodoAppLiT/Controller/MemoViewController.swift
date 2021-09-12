7890a767b71e3455eab90c28844789cf55668167
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2021-09-12 18:57:24 +0900

    PickerViewの実装

--- TodoAppLiT/Controller/MemoViewController.swift
+++ TodoAppLiT/Controller/MemoViewController.swift
@@ -13,8 +13,29 @@ class MemoViewController: UIViewController {
     @IBOutlet weak var detailTextField: UITextField!
     @IBOutlet weak var dateTextField: UITextField!
     
+    var datePicker: UIDatePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // ピッカーの設定
+        datePicker.datePickerMode = UIDatePicker.Mode.dateAndTime
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.locale = Locale.current
+        dateTextField.inputView = datePicker
+        
+        // 決定バーの生成
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35))
+        let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneDate))
+        toolbar.setItems([spacelItem, doneItem], animated: true)
+        
+        // インプットビューの設定
+        dateTextField.inputView = datePicker
+        dateTextField.inputAccessoryView = toolbar
+    }
+    
+
+    @objc func doneDate() {
         
     }
     
