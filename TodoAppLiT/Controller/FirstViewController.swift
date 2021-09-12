ab0da39fb0d916417faa88ad89b706ddbf59810c
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2021-09-12 22:08:23 +0900

    ログイン機能改善

--- TodoAppLiT/Controller/FirstViewController.swift
+++ TodoAppLiT/Controller/FirstViewController.swift
@@ -6,15 +6,16 @@
 //
 
 import UIKit
+import Firebase
 
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    let currentUser = Auth.auth().currentUser
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
-        
         // 毎回TableViewのデータをリロードする
         tableView.reloadData()
     }
@@ -45,6 +46,34 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     // セルのスワイプで削除
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        if currentUser != nil {
+            let dialog = UIAlertController(title: "ログアウト", message: "本当にログアウトしますか？", preferredStyle: .alert)
+            dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+                // ②ログアウトの処理
+                do {
+                    try Auth.auth().signOut()
+                    // ③成功した場合はログイン画面へ遷移
+                    self.dismiss(animated: true, completion: nil)
+                } catch let error as NSError {
+                    print("ログアウト処理失敗", error.localizedDescription)
+                    // ②が失敗した場合
+                    let dialog = UIAlertController(title: "ログアウト失敗", message: error.localizedDescription, preferredStyle: .alert)
+                    dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                }
+            }))
+            dialog.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
+            
+            present(dialog, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
